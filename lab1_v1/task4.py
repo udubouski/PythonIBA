dcd61d3775d04dd2ffb5519370697736fd7f77f4
Uladzislau Dubouski <udubouski@gmail.com>
2019-08-01 08:29:11 +0300

    Task 4

---
+++ lab1_v1/task4.py
@@ -0,0 +1,14 @@
+import random
+listA = []
+
+n = int(input("Input size of list: "))
+for i in range(n):
+    elem = random.random();
+    listA.append(elem)
+
+print("ListA: ", listA)
+
+x = int(input("Input index of element: "))
+listA.pop(x)
+
+print("ListA: ", listA)
